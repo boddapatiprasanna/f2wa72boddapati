71c4abf6d2c6196cdb5f880d881b8d82752beab3
boddapatiprasanna <boddapatiprasanna5@gmail.com>
2022-10-26 15:23:56 -0500

    commit after part 5

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mydata', mydataRouter);
+app.use('/mydata', mydatagitRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
